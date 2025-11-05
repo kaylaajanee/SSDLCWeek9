04d990ad96573823f3475787181b542584df9aa1
kaylaajanee <antoinetta.ajani@student.sgu.ac.id>
2025-11-05 15:38:06 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+const { add, divide } = require("./math.js");
+
+try {
+  assert.strictEqual(add(2, 3), 10);
+  assert.strictEqual(add(-1, 1), 0);
+
+  assert.strictEqual(divide(6, 3), 2);
+  assert.throws(() => divide(6, 0), /Division by zero/);
+
+  console.log("All tests passed");
+} catch (error) {
+  console.log("Test failed:", error.message);
+  process.exit(1);
+}

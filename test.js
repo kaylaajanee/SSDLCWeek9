43c81472b2e0698ac166578caa694c17dc13ded3
kaylaajanee <antoinetta.ajani@student.sgu.ac.id>
2025-11-05 15:54:34 +0700

    change 1 task case

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const { add, divide } = require("./math.js");
 
 try {
-  assert.strictEqual(add(2, 3), 10);
+  assert.strictEqual(add(2, 3), 5);
   assert.strictEqual(add(-1, 1), 0);
 
   assert.strictEqual(divide(6, 3), 2);
